d451025dd4bbd0a6758163c488f4a68764443893
Eduardo Escaleira Fonseca <eescalei@c1r8s2.42porto.com>
2023-05-12 23:57:57 +0100

    m

---
+++ rush00/ex00/rush00.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eescalei <eescalei@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/05 14:48:02 by dduarte-          #+#    #+#             */
+/*   Updated: 2023/02/05 15:47:24 by eescalei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	print(int x, char inicial, char meio, char final)
+{
+	int	cont;
+
+	cont = 1;
+	while (cont <= x)
+	{
+		if (cont == 1)
+			ft_putchar(inicial);
+		else if (cont == x)
+			ft_putchar(final);
+		else
+			ft_putchar(meio);
+	cont++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	contl;
+
+	contl = 1;
+	if (x > 0 && y > 0)
+	{
+		while (contl <= y)
+		{
+			if (contl == 1)
+				print(x, 'o', '-', 'o');
+			else if (contl == y)
+				print(x, 'o', '-', 'o');
+			else
+				print(x, '|', ' ', '|');
+		contl++;
+		}
+	}
+}
---
+++ rush00/ex00/rush02.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eescalei <eescalei@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/05 14:52:15 by dduarte-          #+#    #+#             */
+/*   Updated: 2023/02/05 15:48:23 by eescalei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	print(int x, char inicial, char meio, char final)
+{
+	int	cont;
+
+	cont = 1;
+	while (cont <= x)
+	{
+		if (cont == 1)
+			ft_putchar(inicial);
+		else if (cont == x)
+			ft_putchar(final);
+		else
+			ft_putchar(meio);
+	cont++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	contl;
+
+	contl = 1;
+	if (x > 0 && y > 0)
+	{
+		while (contl <= y)
+		{
+			if (contl == 1)
+				print(x, 'A', 'B', 'A');
+			else if (contl == y)
+				print(x, 'C', 'B', 'C');
+			else
+				print(x, 'B', ' ', 'B');
+		contl++;
+		}
+	}
+}
